4576df460cf61ab8edfc42d45c85e7df3707a24c
dpv927 <dpv927@inlumine.ual.es>
2022-10-31 17:58:26 +0100

    js functions

--- javascript/main.js
+++ javascript/main.js
@@ -1,5 +1,20 @@
-function init_comment() { 
-    typed_color = document.getElementById('input_1').value;
-    about = document.getElementById("comment");
-    about.style.color = "#"+typed_color;
+function setColor(members, color){
+    for(let index = 0 ; index < members.length ; ++index)
+        members[index].style.color  = "#" + color;
+}
+
+function init_input(input_number,input_id){
+    var typed_color = document.querySelector("#input_" + input_number).value;
+    var sources = document.querySelectorAll("#" + input_id);
+    setColor(sources,typed_color);
+}
+
+function init_for(){
+    var color = document.querySelector("#input_29").value;
+    document.querySelector("div").style.color = "#" + color;
+}
+
+function init_back(){
+    var color = document.querySelector("#input_30").value;
+    document.querySelector("div") = "#" + color;
 }
